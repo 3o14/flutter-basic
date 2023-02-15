4a0a8cd03cf16d4653dde03ed74a69d79dc12e94
3o14 <dldnjswn4265@gmail.com>
2023-02-16 00:50:37 +0900

    #3.0 Widget option 사용법

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,51 @@ void main() {
 
 // StatelessWidget : 세 가지 core 위젯 중 하나
 class App extends StatelessWidget {
-  
   // 'build' 입력 후 자동완성
   @override
   Widget build(BuildContext context) {
-
     // Meterial : ios디자인
     // Cupertino : google디자인
     return MaterialApp(
-      home: Scaffold( // Scaffold : 화면 구성 및 구조에 관한 위젯
-        // appBar : 상단 헤더바
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
+      home: Scaffold(
+        // Scaffold : 화면 구성 및 구조에 관한 위젯
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
